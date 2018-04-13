eec423cca1ca8f629eaca6fccfaa57dce36879eb
kibitan <uzukifirst@gmail.com>
2018-04-13 11:46:23 +0200

    feat(Masking::DataLine): parsing table_name

--- lib/masking/data_line.rb
+++ lib/masking/data_line.rb
@@ -1,9 +1,18 @@
 module Masking
   class DataLine
-    def initialize(data_line)
+    attr_reader :raw_line, :table_name
+
+    def initialize(raw_line)
+      @raw_line = raw_line
+      PARSE_REGEXP.match(raw_line).tap do |match_data|
+        @table_name = match_data[:table_name]
+      end
     end
 
     def mask
     end
+
+    private
+    PARSE_REGEXP = /INSERT INTO `(?<table_name>.+)` /
   end
 end
