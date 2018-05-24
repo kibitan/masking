ba8e013ec6d1cc4eab25a7ffc49a14bcee4d6ac0
kibitan <uzukifirst@gmail.com>
2018-05-24 22:19:15 +0200

    feat: WIP: implement DataMaskProcessor.process method

--- lib/masking/data_mask_processor.rb
+++ lib/masking/data_mask_processor.rb
@@ -5,17 +5,22 @@ module Masking
   class DataMaskProcessor
     class << self
       def process(sql_insert_statement_line, target_columns: Config::TargetColumns.new)
-        new(sql_insert_statement_line, target_columns: target_columns).process
+        new(sql_insert_statement_line, target_columns: target_columns).send(:process)
       end
     end
 
     private
 
-    attr_reader :target_columns, :sql_insert_statement
+    attr_reader :raw_line, :target_columns, :sql_insert_statement
 
     def initialize(sql_insert_statement_line, target_columns:)
+      @raw_line             = sql_insert_statement_line
       @target_columns       = target_columns
       @sql_insert_statement = SQLInsertStatement.new(sql_insert_statement_line)
+    end
+
+    def process
+      return raw_line unless target_table?
     end
 
     def target_table?
--- spec/masking/data_mask_processor_spec.rb
+++ spec/masking/data_mask_processor_spec.rb
@@ -2,10 +2,27 @@ require "masking/data_mask_processor"
 
 RSpec.describe Masking::DataMaskProcessor do
   describe ".process" do
-    subject { described_class.process(sql_insert_statement_line, config_fixture_path('target_columns.yml')) }
+    subject { described_class.process(sql_insert_statement_line, target_columns: target_columns) }
     let(:sql_insert_statement_line) { sql_insert_statement_fixture }
-    let(:target_columns) { Masking::Config::TargetColumns.new(config_fixture_path('target_columns.yml')) }
+    let(:target_columns) do
+      instance_double(Masking::Config::TargetColumns).tap do |config|
+        allow(config).to receive(:contains?).with(table_name: 'users').and_return(target_columns_stubbed_return)
+      end
+    end
 
+    context 'when input SQLInsertStatement Line is NOT target_table' do
+      let(:target_columns_stubbed_return) { false }
+
+      it { is_expected.to eq(sql_insert_statement_line) }
+    end
+
+    context 'when input SQLInsertStatement Line is target_table' do
+      let(:target_columns_stubbed_return) { true }
+
+      context 'when input SQLInsertStatement Line has target_column' do
+        pending 'not implemented yet'
+      end
+    end
   end
 
   describe '#target_table?' do
