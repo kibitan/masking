dc2557cba1cd0aada5f31e4ec12a6697eab10a0e
kibitan <uzukifirst@gmail.com>
2018-05-26 18:18:01 +0200

    feat: fix Config::TargetColumns::Table argument as required

--- lib/masking/config/target_columns/table.rb
+++ lib/masking/config/target_columns/table.rb
@@ -6,7 +6,7 @@ module Masking
       class Table
         attr_reader :name, :columns
 
-        def initialize(name, columns: [])
+        def initialize(name, columns:)
           @name = name.to_sym
           @columns = columns.map { |column| Masking::Config::TargetColumns::Column.new(column) }
         end
--- spec/masking/config/target_columns/table_spec.rb
+++ spec/masking/config/target_columns/table_spec.rb
@@ -1,22 +1,23 @@
 require "masking/config/target_columns/table"
 
 RSpec.describe Masking::Config::TargetColumns::Table do
+  let(:name) { 'sample_table' }
+  let(:columns) { %w(column_a column_b column_c) }
+
   describe '#name' do
-    subject { described_class.new(name).name }
-    let(:name) { 'sample_table' }
+    subject { described_class.new(name, columns: columns).name }
 
     it { is_expected.to eq :sample_table }
   end
 
   describe '#columns' do
-    subject { described_class.new(:table_name, columns: columns).columns }
-    let(:columns) { %w(column_a column_b column_c) }
+    subject { described_class.new(name, columns: columns).columns }
 
     it do
       is_expected.to match_array [
-         Masking::Config::TargetColumns::Column.new('column_a'),
-         Masking::Config::TargetColumns::Column.new('column_b'),
-         Masking::Config::TargetColumns::Column.new('column_c')
+        Masking::Config::TargetColumns::Column.new('column_a'),
+        Masking::Config::TargetColumns::Column.new('column_b'),
+        Masking::Config::TargetColumns::Column.new('column_c')
       ]
     end
   end
