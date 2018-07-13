d0ee76f33f5b8e5295b7ed90e14b37fee65bc211
kibitan <uzukifirst@gmail.com>
2018-07-13 13:35:23 +0200

    fix: fix time format

--- lib/masking/config/target_columns/method/time.rb
+++ lib/masking/config/target_columns/method/time.rb
@@ -8,12 +8,17 @@ module Masking
           end
 
           def call
-            time.strftime(FORMAT)
+            "'#{time_format}'"
           end
 
           private
+
           attr_reader :time
+
           FORMAT = "%Y-%m-%d %H:%M:%S".freeze
+          def time_format
+            time.strftime(FORMAT)
+          end
         end
       end
     end
--- spec/masking/config/target_columns/method/time_spec.rb
+++ spec/masking/config/target_columns/method/time_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Masking::Config::TargetColumns::Method::Time do
     context 'when 2018-03-20 18:15:30' do
       let(:value) { Time.new(2018, 3, 20, 18, 15, 30) }
 
-      it { is_expected.to eq "2018-03-20 18:15:30" }
+      it { is_expected.to eq "'2018-03-20 18:15:30'" }
     end
   end
 end
