2218e27bd22b173a1de0178c4771cdd56b335ccf
kibitan <uzukifirst@gmail.com>
2018-12-30 23:54:53 +0100

    fix: temporary commit

--- lib/masking/insert_statement.rb
+++ lib/masking/insert_statement.rb
@@ -43,7 +43,7 @@ module Masking
     #   if there is single quote inside of value, it will dumped with escape. e.g. 'chikahiro\'s item'
     #   in number, there could be include Scientific notation e.g. 1.2E3 / -1.2E-3 / 1e+030 / 9.71726e-17
     #     refs: https://dev.mysql.com/doc/refman/5.7/en/precision-math-numbers.html
-    VALUE_REGEXP = "([+eE0-9.-]+|_binary '.*?'|'.*?'|NULL)"
+    VALUE_REGEXP = %q{([+eE0-9.-]+|_binary '(?~\'\)).*?'|'.*?'|NULL)}
 
     def values_regexp
       /\(#{([VALUE_REGEXP] * columns.count).join(?,)}\),?/
