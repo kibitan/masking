d9d4103047bf13f57d156d9f6309662426660ebb
kibitan <uzukifirst@gmail.com>
2019-10-31 18:58:00 +0100

    adding acceptance test

---
+++ acceptance/run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -Ceu -o pipefail
+set -vx
+
+FILEDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+## clear tmp file
+rm $FILEDIR/tmp/*
+
+## import database
+mysql -h mysqldb -u mysqluser -ppassword mydb < $FILEDIR/import_dumpfile.sql
+
+## masking
+mysqldump -h mysqldb -u mysqluser -ppassword mydb --complete-insert | exe/masking -c $FILEDIR/masking.yml > $FILEDIR/tmp/masking_dumpfile.sql
+mysql -h mysqldb -u mysqluser -ppassword mydb < $FILEDIR/tmp/masking_dumpfile.sql
+
+## compare
+mysql -h mysqldb -u mysqluser -ppassword mydb -e 'SELECT * FROM users ORDER BY id;' --table > $FILEDIR/tmp/query_result.txt
+diff $FILEDIR/tmp/query_result.txt $FILEDIR/expected_query_result.txt
