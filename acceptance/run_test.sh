8d6e3d5bf828f1f9b0e0b1a7e42c4072acaade34
kibitan <uzukifirst@gmail.com>
2019-11-03 12:00:56 +0100

    refactor: refactoring test file

--- acceptance/run_test.sh
+++ acceptance/run_test.sh
@@ -3,18 +3,23 @@
 set -Ceu -o pipefail
 set -vx
 
+MYSQL_HOST=${MYSQL_HOST:-mysqldb}
+MYSQL_USER=${MYSQL_USER:-mysqluser}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-password}
+MYSQL_DBNAME=${MYSQL_DBNAME:-mydb}
+
 FILEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 ## clear tmp file
-rm $FILEDIR/tmp/*
+rm "$FILEDIR"/tmp/* || echo 'no tmp file'
 
 ## import database
-mysql -h mysqldb -u mysqluser -ppassword mydb < $FILEDIR/import_dumpfile.sql
+mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/import_dumpfile.sql"
 
 ## masking
-mysqldump -h mysqldb -u mysqluser -ppassword mydb --complete-insert | exe/masking -c $FILEDIR/masking.yml > $FILEDIR/tmp/masking_dumpfile.sql
-mysql -h mysqldb -u mysqluser -ppassword mydb < $FILEDIR/tmp/masking_dumpfile.sql
+mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
+mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/tmp/masking_dumpfile.sql"
 
 ## compare
-mysql -h mysqldb -u mysqluser -ppassword mydb -e 'SELECT * FROM users ORDER BY id;' --vertical > $FILEDIR/tmp/query_result.txt
-diff $FILEDIR/tmp/query_result.txt $FILEDIR/expected_query_result.txt
+mysql -h mysqldb -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" -e 'SELECT * FROM users ORDER BY id;' --vertical > "$FILEDIR/tmp/query_result.txt"
+diff "$FILEDIR/tmp/query_result.txt" "$FILEDIR/expected_query_result.txt"
