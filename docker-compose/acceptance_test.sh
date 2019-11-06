277693c6f4132f164ad0d31feb24e0e7d180b60c
kibitan <uzukifirst@gmail.com>
2019-11-06 17:16:24 +0100

    fix script for bash

--- docker-compose/acceptance_test.sh
+++ docker-compose/acceptance_test.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -Ceu -o pipefail
-# set -vx # for debug
+#set -vx # for debug
 
 MYSQL_VERSION=${1:-mysql80}
 docker-compose -f docker-compose.yml -f "docker-compose/$MYSQL_VERSION.yml" run -e "MYSQL_HOST=$MYSQL_VERSION" app acceptance/run_test.sh
