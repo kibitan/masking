e668c977069deafe0f1fbecadef6b06033ae7f7c
Chikahiro Tokoro <uzukifirst@gmail.com>
2019-11-09 11:00:00 +0100

    Fix acceptance test mistake (#39)
    * fix typo
    
    * fix schema mistake

--- acceptance/import_dumpfile.sql
+++ acceptance/import_dumpfile.sql
@@ -24,9 +24,9 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `string` varchar(11) NOT NULL DEFAULT '',
+  `string` varchar(20) NOT NULL DEFAULT '',
   `email` varchar(200) NOT NULL DEFAULT '',
-  `inteager` int(11) DEFAULT NULL,
+  `integer` int(11) DEFAULT NULL,
   `float` float DEFAULT NULL,
   `boolean` tinyint(1) DEFAULT NULL,
   `null` int(11) DEFAULT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `string`, `email`, `inteager`, `float`, `boolean`, `null`, `date`, `time`, `binary_or_blob`) VALUES (1,'example@exa','test@example.com',1245,2.1,1,-321,NULL,NULL,_binary '\\x92\0\0\0\0\0\0\0'),(2,'あいうえお','invalid@email',0,-23.4422,0,321,NULL,NULL,_binary '\\x92\\x92\0\0\0'),(3,'+-l;a*&^%$','chikahiro@test.com',-1,21.2321,NULL,-231321,'2019-10-31','2019-10-31 16:27:21',NULL);
+INSERT INTO `users` (`id`, `string`, `email`, `integer`, `float`, `boolean`, `null`, `date`, `time`, `binary_or_blob`) VALUES (1,'example@exa','test@example.com',1245,2.1,1,-321,NULL,NULL,_binary '\\x92\0\0\0\0\0\0\0'),(2,'あいうえお','invalid@email',0,-23.4422,0,321,NULL,NULL,_binary '\\x92\\x92\0\0\0'),(3,'+-l;a*&^%$','chikahiro@test.com',-1,21.2321,NULL,-231321,'2019-10-31','2019-10-31 16:27:21',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
