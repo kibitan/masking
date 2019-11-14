bf5d4c3e100253db5cfaa2675156e47d7bcc8ee7
kibitan <uzukifirst@gmail.com>
2019-11-14 17:26:47 +0100

    simplify

--- spec/masking/data_mask_processor_spec.rb
+++ spec/masking/data_mask_processor_spec.rb
@@ -7,6 +7,7 @@ require 'masking/data_mask_processor'
 RSpec.describe Masking::DataMaskProcessor do
   describe '.process' do
     subject { described_class.process(insert_statement_line, target_columns: target_columns) }
+    # TODO: use mock instead of real object or refactoring
     let(:target_columns) { Masking::Config::TargetColumns.new(config_fixture_path) }
 
     context 'when input InsertStatement Line is NOT target_table' do
@@ -32,21 +33,18 @@ RSpec.describe Masking::DataMaskProcessor do
 
   describe '#target_table?' do
     subject { described_class.new(insert_statement_line, target_columns: target_columns).send(:target_table?) }
+
     let(:insert_statement_line) { insert_statement_fixture }
-    let(:target_columns) do
-      instance_double(Masking::Config::TargetColumns).tap do |config|
-        allow(config).to receive(:contains?).with(table_name: 'users').and_return(target_columns_stubbed_return)
-      end
-    end
+    let(:target_columns) { instance_double(Masking::Config::TargetColumns, 'contains?': stub_target_columns_return) }
 
     context 'table is defined in target_columns' do
-      let(:target_columns_stubbed_return) { true }
+      let(:stub_target_columns_return) { true }
 
       it { is_expected.to be true }
     end
 
     context 'table is NOT defined in target_columns' do
-      let(:target_columns_stubbed_return) { false }
+      let(:stub_target_columns_return) { false }
 
       it { is_expected.to be false }
     end
