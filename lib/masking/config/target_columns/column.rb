3a82b40aede6bc7a50db0c3a5da5e3b031e97318
kibitan <uzukifirst@gmail.com>
2023-04-01 15:38:21 +0200

    feat: 🎸 check the method for ignore null option

--- lib/masking/config/target_columns/column.rb
+++ lib/masking/config/target_columns/column.rb
@@ -6,22 +6,36 @@ module Masking
   class Config
     class TargetColumns
       class Column
-        attr_reader :name, :table_name, :method_value, :method
+        attr_reader :table_name, :method_value, :method
 
         def initialize(name, table_name:, method_value:)
           raise ColumnNameIsNil if name.nil?
 
-          @name         = name.to_sym
-          @table_name   = table_name.to_sym
-          @method_value = method_value
-          @method       = Method.new(method_value)
+          @original_name = name.to_sym
+          @table_name    = table_name.to_sym
+          @method_value  = method_value
+          @method        = Method.new(method_value)
         end
 
         def ==(other)
           name == other.name && table_name == other.table_name && method_value == other.method_value
         end
 
+        def name
+          @name ||= ignore_null? ? original_name.to_s.chomp(IGNORE_NULL_SUFFIX).to_sym : original_name
+        end
+
+        def ignore_null?
+          @ignore_null ||= original_name.to_s.end_with?(IGNORE_NULL_SUFFIX)
+        end
+
         class ColumnNameIsNil < StandardError; end
+
+        private
+
+        attr_reader :original_name
+
+        IGNORE_NULL_SUFFIX = '?'
       end
     end
   end
---
+++ lib/masking/config/target_columns/column/ignore_null.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Masking
+  class Config
+    class TargetColumns
+      class Column
+        module IgnoreNull
+        end
+      end
+    end
+  end
+end
