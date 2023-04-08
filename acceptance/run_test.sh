fcaf1f5db474442e47e9b58b41033a9f94438e5a
kibitan <uzukifirst@gmail.com>
2023-04-08 14:20:33 +0200

    test: 💍 add test for error message
    (but not reproduced in local..)

--- acceptance/run_test.sh
+++ acceptance/run_test.sh
@@ -16,10 +16,22 @@ rm "$FILEDIR"/tmp/* || echo 'no tmp file'
 ## import database
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/import_dumpfile.sql"
 
-## masking
+## masking & restore
 mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/tmp/masking_dumpfile.sql"
 
-## compare
+## compare the result
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" -e 'SELECT * FROM users ORDER BY id;' --vertical > "$FILEDIR/tmp/query_result.txt"
-diff "$FILEDIR/expected_query_result.txt" "$FILEDIR/tmp/query_result.txt"  && echo 'test passed!'
+diff "$FILEDIR/expected_query_result.txt" "$FILEDIR/tmp/query_result.txt" || (echo 'output compare test failed' && exit 1)
+
+## test errors
+set +e
+# without masking.yml
+mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/no_file.yml" 2> "$FILEDIR/tmp/errors.txt" 1> /dev/null
+# without `--complete-insert``
+mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" | exe/masking -c "$FILEDIR/masking.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
+set -e
+diff "$FILEDIR/expected_error_result.txt" "$FILEDIR/tmp/errors.txt" || (echo 'output compare test failed' && exit 1)
+
+echo 'test passed!'
+exit 0
