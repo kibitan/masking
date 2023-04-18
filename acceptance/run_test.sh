bc7fdfa6af0aa3a30323a171c52b78bd74e68487
kibitan <uzukifirst@gmail.com>
2023-04-18 21:59:05 +0200

    refactor: 💡 changeable execution command by enviroment variable

--- acceptance/run_test.sh
+++ acceptance/run_test.sh
@@ -9,6 +9,7 @@ MYSQL_PASSWORD=${MYSQL_PASSWORD:-password}
 MYSQL_DBNAME=${MYSQL_DBNAME:-mydb}
 
 FILEDIR="$( cd "$( dirname "$0" )" && pwd )"
+EXE_COMMAND=${EXE_COMMAND:-"masking"}
 
 # clear tmp file
 rm "$FILEDIR"/tmp/* || echo 'no tmp file'
@@ -17,7 +18,7 @@ rm "$FILEDIR"/tmp/* || echo 'no tmp file'
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/import_dumpfile.sql"
 
 #  masking & restore
-mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
+mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | $EXE_COMMAND -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/tmp/masking_dumpfile.sql"
 ## compare the result
 mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" -e 'SELECT * FROM users ORDER BY id;' --vertical > "$FILEDIR/tmp/query_result.txt"
@@ -26,9 +27,9 @@ diff "$FILEDIR/expected_query_result.txt" "$FILEDIR/tmp/query_result.txt" || (ec
 # test errors
 set +e
 ## without masking.yml
-mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/no_file.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
+mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | $EXE_COMMAND -c "$FILEDIR/no_file.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
 ## without `--complete-insert``
-mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" | exe/masking -c "$FILEDIR/masking.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
+mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" | $EXE_COMMAND -c "$FILEDIR/masking.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
 set -e
 ### compare the result
 diff "$FILEDIR/expected_error_result.txt" "$FILEDIR/tmp/errors.txt" || (echo 'error output test failed' && exit 1)
