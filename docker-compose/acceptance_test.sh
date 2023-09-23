6f24da637ca5dfbf5f1d76e5b21a973c44d92d8e
kibitan <uzukifirst@gmail.com>
2023-09-23 16:10:40 +0200

    fix: 🐛 unbound value error

--- docker-compose/acceptance_test.sh
+++ docker-compose/acceptance_test.sh
@@ -10,6 +10,7 @@ cd "$(dirname "$0")"
 
 MYSQL_VERSION=${1:-mysql80}
 DOCKER_COMPOSE_FILE=${2:-docker-compose.yml}
+TRACE=${TRACE:-0}
 
 main() {
   docker-compose -f "../$DOCKER_COMPOSE_FILE" -f "./$MYSQL_VERSION.yml" run -e "MYSQL_HOST=$MYSQL_VERSION" -e "TRACE=$TRACE" app acceptance/run_test.sh
