cd937b1836f7a945cea01b5be19a66b48be4a5bc
kibitan <uzukifirst@gmail.com>
2023-09-23 17:23:36 +0200

    ci: 🎡 change to use root user

--- acceptance/run_test.sh
+++ acceptance/run_test.sh
@@ -7,21 +7,16 @@ if [[ "${TRACE-0}" == "1" ]]; then
 fi
 
 MYSQL_HOST=${MYSQL_HOST:-localhost}
-MYSQL_USER=${MYSQL_USER:-mysqluser}
-MYSQL_PASSWORD=${MYSQL_PASSWORD:-password}
-MYSQL_DBNAME=${MYSQL_DBNAME:-mydb}
+MYSQL_USER=${MYSQL_USER:-root}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-root_password}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-MYSQL_ROOT_PASSWORD}
+MYSQL_DBNAME=${MYSQL_DBNAME:-mydb}
 
 FILEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 main() {
   # clear tmp file
   rm "$FILEDIR"/tmp/* || echo 'no tmp file'
-
-  # setup database user, relates following breaking change:
-  #  * https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-21.html#mysqld-8-0-21-security
-  #  * https://dev.mysql.com/doc/refman/5.7/en/privileges-provided.html#priv_process
-  mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT PROCESS ON *.* TO '$MYSQL_USER'@'%';"
 
   # import database
   mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/import_dumpfile.sql"
