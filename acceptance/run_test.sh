64fbfcfa8bcbc0c65214141e7fd728d1384986a8
kibitan <uzukifirst@gmail.com>
2024-12-30 14:28:26 +0100

    refactor: 💡 add MYSQL_OPTION for acceptance test

--- acceptance/run_test.sh
+++ acceptance/run_test.sh
@@ -12,6 +12,8 @@ MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-root_password}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-MYSQL_ROOT_PASSWORD}
 MYSQL_DBNAME=${MYSQL_DBNAME:-mydb}
 MYSQL_ANONYMIZED_DBNAME=${MYSQL_ANONYMIZED_DBNAME:-anonnymized_mydb}
+MYSQL_OPTIONS=${MYSQL_OPTIONS:-}
+_MYSQL_COMMAND_OPTIONS="--host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_OPTIONS"
 
 FILEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
@@ -20,25 +22,25 @@ main() {
   rm "$FILEDIR"/tmp/* || echo 'no tmp file'
 
   # import database
-  mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" < "$FILEDIR/import_dumpfile.sql"
+  mysql ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_DBNAME" < "$FILEDIR/import_dumpfile.sql"
 
   #  masking & restore
   ## TODO: temporary add `--skip-extended-insert` as not working now
-  # mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
-  mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert --skip-extended-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
+  # mysqldump ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
+  mysqldump ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_DBNAME" --complete-insert --skip-extended-insert | exe/masking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/masking_dumpfile.sql"
 
-  mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE $MYSQL_ANONYMIZED_DBNAME;"
-  mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_ANONYMIZED_DBNAME" < "$FILEDIR/tmp/masking_dumpfile.sql"
+  mysql ${_MYSQL_COMMAND_OPTIONS} -e "CREATE DATABASE $MYSQL_ANONYMIZED_DBNAME;"
+  mysql ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_ANONYMIZED_DBNAME" < "$FILEDIR/tmp/masking_dumpfile.sql"
   ## compare the result
-  mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_ANONYMIZED_DBNAME" -e 'SELECT * FROM users ORDER BY id;' --vertical > "$FILEDIR/tmp/query_result.txt"
+  mysql ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_ANONYMIZED_DBNAME" -e 'SELECT * FROM users ORDER BY id;' --vertical > "$FILEDIR/tmp/query_result.txt"
   diff "$FILEDIR/expected_query_result.txt" "$FILEDIR/tmp/query_result.txt" || (echo 'test failed' && exit 1)
 
   # test errors
   set +e
   ## without masking.yml
-  mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/no_file.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
+  mysqldump ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_DBNAME" --complete-insert | exe/masking -c "$FILEDIR/no_file.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
   ## without `--complete-insert``
-  mysqldump -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DBNAME" | exe/masking -c "$FILEDIR/masking.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
+  mysqldump ${_MYSQL_COMMAND_OPTIONS} "$MYSQL_DBNAME" | exe/masking -c "$FILEDIR/masking.yml" 2>> "$FILEDIR/tmp/errors.txt" 1> /dev/null
   set -e
   ### compare the result
   diff "$FILEDIR/expected_error_result.txt" "$FILEDIR/tmp/errors.txt" || (echo 'error output test failed' && exit 1)
