17144f5228390f1ab20c470149e288b294d749fa
kibitan <uzukifirst@gmail.com>
2024-12-30 14:59:30 +0100

    fix: 🐛 add mysqloption for wait

--- docker-compose/wait-for-mysql.sh
+++ docker-compose/wait-for-mysql.sh
@@ -5,12 +5,13 @@ set -e
 
 MYSQL_USER=${MYSQL_USER:-root}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-password}
+MYSQL_OPTIONS=${MYSQL_OPTIONS:-}
 
 MYSQL_HOST="$1"
 shift
 cmd=$*
 
-until mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e 'exit'; do
+until mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_OPTIONS -e 'exit'; do
   >&2 echo "mysql is unavailable - sleeping"
   sleep 1
 done
