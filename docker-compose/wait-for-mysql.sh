6abe01e1723e6411bb40bf28b11c509d8a3ce53c
kibitan <uzukifirst@gmail.com>
2024-12-30 15:05:51 +0100

    ci: 🎡 add MYSQL_OPTION for docker compose

--- docker-compose/wait-for-mysql.sh
+++ docker-compose/wait-for-mysql.sh
@@ -11,7 +11,7 @@ MYSQL_HOST="$1"
 shift
 cmd=$*
 
-until mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --ssl-mode=DISABLED -e 'exit'; do
+until mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_OPTIONS -e 'exit'; do
   >&2 echo "mysql is unavailable - sleeping"
   sleep 1
 done
